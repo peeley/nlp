Fixed padding of sentences to fix loss calculation bug.

dataUtils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import unicodedata, string, torch, langModel, langModel, random
 import torch.utils.data
+import torch.nn.utils.rnn as rnn
 import nltk
 
 all_letters = string.ascii_letters + " .,;'-"
@@ -18,9 +19,8 @@ class LangDataset(torch.utils.data.Dataset):
         return len(self.frame)
 
     def __getitem__(self, idx):
-        testLine = langModel.normalize(self.frame.loc[idx, self.testLang.name])
-        testLine = ' '.join(nltk.word_tokenize(testLine))
-        targetLine = langModel.normalize(self.frame.loc[idx, self.targetLang.name])
+        testLine   = self.frame.loc[idx, self.testLang.name]
+        targetLine = self.frame.loc[idx, self.targetLang.name]
         testTensor, targetTensor = langModel.tensorFromPair(self.testLang, self.targetLang, 
                                                             testLine, targetLine,
                                                             self.length)
@@ -49,6 +49,25 @@ def loadTrainingData(vocabSize, words, testFilename, targetFilename, testLang, t
     print('Creation complete, ', index, ' lines.')
     targetFile.close()
     testFile.close()
+    dataset = LangDataset(frame, testLang, targetLang, words)
+    return dataset
+
+def loadToyData(vocabSize, words, filename, testLang, targetLang):
+    frame = pd.DataFrame(columns = [testLang.name, targetLang.name])
+    index = 0
+    with open(filename, encoding = 'utf8') as langFile:
+        for line in langFile:
+            if index == vocabSize:
+                break
+            sentences = line.strip('\n').split('\t')
+            testLine = ' '.join(nltk.word_tokenize((sentences[0])))
+            targetLine = ' '.join(nltk.word_tokenize((sentences[1])))
+            testLang.addSentence(testLine)
+            targetLang.addSentence(targetLine)
+
+            frame.loc[index, targetLang.name] = targetLine
+            frame.loc[index, testLang.name] = testLine
+            index += 1
     dataset = LangDataset(frame, testLang, targetLang, words)
     return dataset
 

evaluateSeq2Seq.py
@@ -21,7 +21,7 @@ else:
 def evaluate(encoder, decoder, rawString, testLang, targetLang, train = False):
     with torch.no_grad():
         for item in range(len(rawString)):
-            inputString = langModel.normalize(rawString[item])
+            inputString = (rawString[item])
             print('\nTest sentence: \t', inputString)
             inputSentence, rareWords = langModel.tensorFromSentence(testLang, inputString, length)
             inputSentence = inputSentence.view(1,-1,1).to(device)
@@ -43,7 +43,7 @@ def evaluate(encoder, decoder, rawString, testLang, targetLang, train = False):
                     decoderOutput, decoderHidden = decoder(decoderInput, decoderHidden, encoderOutputs)
                     decoderOutput = decoderOutput.view(1, -1)
                     topv, topi = decoderOutput.data.topk(1)
-                    if topi.item() == 1:
+                    if topi.item() == testLang.EOS:
                         decodedWords.append('/end/')
                         break
                     else:
@@ -61,7 +61,7 @@ def testBLEU(testData, encoder, decoder, testLang, targetLang):
         print('--- TESTING BLEU SCORES ---')
         for index, line in testData.iterrows():
             testLine    = line[testLang.name]
-            targetLine  = line[targetLang.name]
+            targetLine  = langModel.normalize(line[targetLang.name])
             print('Item: \t#{}/{}'.format(index, testData.shape[0]))
             decodedString = evaluate(encoder, decoder, [testLine], testLang, targetLang)
             if '' in decodedString:

langModel.py
@@ -4,12 +4,13 @@ import numpy as np
 class langModel:
     def __init__(self, name):
         self.name = name
-        self.word2idx = {'/start/': 0, '/end/':1}
+        self.word2idx = {0:'/pad/', '/start/': 1, '/end/':2}
         self.word2count = {}
-        self.idx2word = {0: '/start/', 1:'/end/'}
+        self.idx2word = {0: '/pad/', 1:'/start/', 2:'/end/'}
         self.nWords = 2
-        self.EOS = 1
-        self.SOS = 0
+        self.EOS = 2
+        self.SOS = 1
+        self.PAD = 0
         self.glove = []
 
     def addEmbedding(self, filepath, filename):
@@ -63,11 +64,11 @@ def tensorFromSentence(lang, sentence, length):
             indices.append(lang.word2idx[word])
         except KeyError as e:
             rareWords[num] = word
-            print('WARNING - Word not in vocabulary: ', word)
-    indices.append(1)
-    while len(indices) < (length + 15):
-        indices.append(1)
-    indices = torch.tensor(indices, dtype = torch.long).view(-1,1)
+            print('WARNING - Word not in vocabulary: "{}"'.format(word))
+    indices.append(lang.EOS)
+    while len(indices) < (length):
+        indices.append(lang.PAD)
+    indices = torch.tensor(indices, dtype = torch.long).view(-1, 1)
     return indices, rareWords
 
 def tensorFromPair(inputLang, outputLang, inputSentence, outputSentence, length):

seq2seq.py
@@ -1,5 +1,6 @@
 import torch, torchtext, pickle
 import torch.nn as nn
+import torch.nn.utils.rnn as rnn
 
 class encoder(nn.Module):
     def __init__(self, inputSize, hiddenSize = 300, lr = 1e-3, numLayers = 1):
@@ -7,12 +8,12 @@ class encoder(nn.Module):
         self.hiddenSize = hiddenSize
         self.inputSize = inputSize
         self.numLayers = numLayers
-        self.embedding = nn.Embedding(self.inputSize, self.hiddenSize)
+        self.embedding = nn.Embedding(self.inputSize, self.hiddenSize, padding_idx = 0)
         self.lstm = nn.LSTM(self.hiddenSize, self.hiddenSize, bidirectional = True, num_layers=numLayers, batch_first = True)
         self.lr = lr
 
     def forward(self, input, hidden):
-        batchSize = input.shape[0]
+        batchSize= input.shape[0]
         embed = self.embedding(input)
         embed = embed.view(batchSize, 1, self.hiddenSize)
         output, hidden = self.lstm(embed, hidden)
@@ -27,16 +28,17 @@ class decoder(nn.Module):
         self.numLayers = numLayers
         self.dropout = nn.Dropout(dropoutProb)
         self.embed = nn.Embedding(self.outputSize, self.hiddenSize)
-        self.lstm = nn.LSTM(self.hiddenSize, self.hiddenSize, num_layers = self.numLayers)
+        self.lstm = nn.LSTM(self.hiddenSize, self.hiddenSize, num_layers = self.numLayers*2, batch_first = True)
         self.linear = nn.Linear(self.hiddenSize, self.outputSize)
-        self.softmax = nn.LogSoftmax(dim = 1)
+        self.softmax = nn.LogSoftmax(dim = 2)
         self.lr = lr
 
     def forward(self, input, hidden, out):
-        embed = self.embed(input).view(1,1,-1)
+        batchSize = input.shape[0]
+        embed = self.embed(input).view(batchSize,1, self.hiddenSize)
         out = nn.functional.relu(embed)
         output, hidden = self.lstm(out, hidden)
-        output = self.linear(output[0])
+        output = self.linear(output[:])
         output = self.softmax(output)
         return output, hidden
 
@@ -48,7 +50,7 @@ class attnDecoder(nn.Module):
         self.outputSize = outputSize
         self.hiddenSize = hiddenSize
         self.numLayers = numLayers
-        self.embed = nn.Embedding(self.outputSize, self.hiddenSize)
+        self.embed = nn.Embedding(self.outputSize, self.hiddenSize, padding_idx=0)
         self.dropout = nn.Dropout(dropoutProb)
         self.attn = nn.Linear(self.hiddenSize*2, self.maxLength)
         self.attnCombine = nn.Linear(self.hiddenSize * 3, self.hiddenSize)

trainSeq2Seq.py
@@ -30,6 +30,7 @@ testDataVal = 'data/de-en/train.tok.clean.bpe.32000.en'
 targetDataVal = 'data/de-en/train.tok.clean.bpe.32000.de'
 #testDataVal = 'data/de-en/newstest2011.tok.bpe.32000.en'
 #targetDataVal = 'data/de-en/newstest2011.tok.bpe.32000.de'
+toyData = 'data/de-en/deu-eng/deu.txt'
 
 testLang = langModel.langModel('eng')
 targetLang = langModel.langModel('ipq')
@@ -38,9 +39,10 @@ if args.reverse:
     testData, targetData = targetData, testData
     testDataVal, targetDataVal = targetDataVal, testDataVal
 
-trainingData = dataUtils.loadTrainingData(args.size, args.dataSentenceLength, testData, targetData, testLang, targetLang)
-testData = dataUtils.loadTestData(500, args.dataSentenceLength, testDataVal, targetDataVal, testLang, targetLang) 
-dataLoader = torch.utils.data.DataLoader(trainingData, shuffle = True, num_workers = 8, batch_size = args.batch)
+#trainingData = dataUtils.loadTrainingData(args.size, args.dataSentenceLength, testData, targetData, testLang, targetLang)
+trainingData = dataUtils.loadToyData(args.size, args.dataSentenceLength, toyData, testLang, targetLang)
+testData = dataUtils.loadTestData(args.size, args.dataSentenceLength, testDataVal, targetDataVal, testLang, targetLang) 
+dataLoader = torch.utils.data.DataLoader(trainingData, shuffle = True, num_workers = 0, batch_size = args.batch)
 
 cuda = False
 if torch.cuda.is_available():
@@ -53,15 +55,15 @@ else:
 
 encoder = seq2seq.encoder(testLang.nWords+1, hiddenSize=args.hSize, 
                           lr = args.learningRate, numLayers = args.layers).to(device)
-decoder = seq2seq.attnDecoder(targetLang.nWords+1, hiddenSize=args.hSize, 
+decoder = seq2seq.decoder(targetLang.nWords+1, hiddenSize=args.hSize, 
                               lr = args.learningRate, dropoutProb = .001, 
                               maxLength=args.maxWords, numLayers = args.layers).to(device)
 
-loss_fn = nn.NLLLoss()
-encoderOptim = torch.optim.Adam(encoder.parameters(), lr= args.learningRate)
-decoderOptim = torch.optim.Adam(decoder.parameters(), lr= args.learningRate)
-#encoderScheduler = torch.optim.lr_scheduler.ExponentialLR(encoderOptim, gamma = .9)
-#decoderScheduler = torch.optim.lr_scheduler.ExponentialLR(decoderOptim, gamma = .9)
+loss_fn = nn.NLLLoss(ignore_index = testLang.PAD, reduction = 'sum')
+#encoderOptim = torch.optim.Adam(encoder.parameters(), lr= args.learningRate)
+#decoderOptim = torch.optim.Adam(decoder.parameters(), lr= args.learningRate)
+encoderOptim = torch.optim.SGD(encoder.parameters(), lr= args.learningRate, momentum = .9, nesterov = True)
+decoderOptim = torch.optim.SGD(decoder.parameters(), lr= args.learningRate, momentum = .9, nesterov = True)
 
 encoder = nn.DataParallel(encoder)
 decoder = nn.DataParallel(decoder)
@@ -79,7 +81,7 @@ for epoch in range(args.epochs):
             trainingData = dataUtils.loadTrainingData(args.size, args.dataSentenceLength, 
                                                       testData, targetData, testLang, targetLang)
             dataLoader = torch.utils.data.DataLoader(trainingData, shuffle = True, 
-                                                     num_workers = numWorkers, batch_size = args.batch)
+                                                     num_workers = 0, batch_size = args.batch)
             print("Created new dataset")
     for row, item in enumerate(dataLoader):
         stepStartTime = datetime.datetime.now()
@@ -87,7 +89,6 @@ for epoch in range(args.epochs):
         batchSize = inputTensor.shape[0]
         inputLine, targetLine = item[2][0], item[3][0]
         loss = 0
-        
         encoderOptim.zero_grad()
         decoderOptim.zero_grad()
         encoderHidden = seq2seq.initHidden(cuda, args.hSize, args.layers*2, batchSize)
@@ -96,7 +97,7 @@ for epoch in range(args.epochs):
             encoderOutput, encoderHidden = encoder(inputTensor[:,inputLetter], encoderHidden)
             encoderOutputs[:, inputLetter] = encoderOutput[:, 0]
         
-        decoderInput = torch.zeros([batchSize, 1], dtype = torch.long).to(device)
+        decoderInput = torch.ones([batchSize, 1], dtype = torch.long).to(device)
         decoderHidden = encoderHidden
 
         teacherForce = True if random.random() < teacherForceRatio else False
@@ -110,7 +111,7 @@ for epoch in range(args.epochs):
                 decoderInput = targetTensor[:, targetLetter]
             else:
                 topv, topi = decoderOutput.topk(1)
-                decoderInput = topi.squeeze().detach()
+                decoderInput = topi.squeeze().detach().view(batchSize, 1)
 
         loss.backward()
         nn.utils.clip_grad_norm_(decoder.parameters(), 10)
@@ -121,7 +122,7 @@ for epoch in range(args.epochs):
         stepTime = datetime.datetime.now() - stepStartTime
     epochLoss = epochLoss / args.size
     epochTime = datetime.datetime.now() - epochTime
-    print('Epoch: {}\t Loss: {:.8}\tEpoch Time: {}\tStep Time: {}'.format(epoch+1, epochLoss, epochTime, stepTime))
+    print('Epoch: {}\t Loss: {:.8} \tEpoch Time: {}\tStep Time: {}'.format(epoch+1, epochLoss, epochTime, stepTime))
     #encoderScheduler.step()
     #decoderScheduler.step()
 endTime = datetime.datetime.now()